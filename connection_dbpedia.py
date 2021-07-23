d08bcb8e3a684d041ca7dc4419e6fcba4d721214
AndresAlvaradow <andresalvarado34@gmail.com>
2021-07-23 02:23:00 -0500

    ImplementacionPLN

--- connection_dbpedia.py
+++ connection_dbpedia.py
@@ -3,30 +3,6 @@ from SPARQLWrapper import SPARQLWrapper, JSON
 sparql = SPARQLWrapper('https://dbpedia.org/sparql')
 
 
-def get_response_dbpedia(item):
-    sparql.setQuery(f'''
-        SELECT ?name ?comment
-        WHERE {{
-            dbr:{item} rdfs:label ?name .
-            dbr:{item} rdfs:comment ?comment .
-            FILTER (lang(?name) = 'es')
-            FILTER (lang(?comment) = 'es')
-        }}
-    ''')
-    sparql.setReturnFormat(JSON)
-    qres = sparql.query().convert()
-
-    if len(qres['results']['bindings']) == 0:
-        # print("---------------------")
-        return "Información no encontrada"
-
-    else:
-        result = qres['results']['bindings'][0]
-        comment = result['comment']['value']
-        # print("---------------------me lleva")
-        return comment
-
-
 def get_response_dbpedia_pizzas():
     sparql.setQuery(f'''
       SELECT ?name ?res  ?image 
@@ -53,6 +29,3 @@ if __name__ == '__main__':
     result = get_response_dbpedia_pizzas()
     for item in result:
         name, comment, image_url = result['name']['value'], result['comment']['value'], result['image']['value']
-        # print ('Nombre de la pizza : ' + name + "\n Descripción : " + comment + "\n" + image_url)
-    # print(get_response_dbpedia(item))
-    # print_response_dbpedia(qres)
--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-from os import name
 from telegram.ext import *
 import logging
 import connection_dbpedia as dbpedia
 import OWLconexion as owl
-
+import PNL_Spacy as pln
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 
 
 
@@ -20,54 +20,29 @@ def error(update, context):
 
 # MENUS
 
-
-
-def second_submenu_1(update, context):
-    query = update.callback_query
-    query.edit_message_text(
-        text="Listo te avisare cuando esté listo")
-
-# COMMANDS
-
-
-def help_command(update, context):
-    update.message.reply_text(
-
-        "\n\nYo soy Kin, tu asistente te apoyare en todo tu pedido"
-        "\n\nPara interartuar utliza el comando /start")
-
-
-
-def ingredients_command(update, context):
-    user_says = " ".join(context.args)
-    # update.message.reply_text("You said: " + user_says)
-    update.message.reply_text(
-        "Los ingredientes de  " + user_says + " son:\n\n"+dbpedia.get_response_dbpedia_ingredients(user_says.capitalize()))
-
-
-
-
-
 def start_command(update, context):
+
     update.message.reply_text(
         'Hola, yo soy Kin :\n\n te ayudara con tu Pedido')
     update.message.reply_text(
-        "Puedes usar los siguientes comandos :\n"
-        #"\n/formenu -> realiza tu pedido mediante un menu (recomendado)"
-        #"\n/help -> muestra mas información de mí Kin"
-        "\n/listPizzaDb -> muestra el listado de pizzas de DBpedia"
-        "\n/PizzaOWL -> muestra el listado de pizzas de OWL Pizza")
-        #"\n/pizza -> muestra las pizzas que puedes buscar"
-       # "\n/ingredients \"nombre de la pizza\" -> permite buscar los ingredientes de la pizza (utiliza el commando /pizza para ver que pizzas puedes buscar)")
+        text='Seleccione una opcion',
+        reply_markup=InlineKeyboardMarkup([
+            [InlineKeyboardButton(text='listPizzaDb', callback_data='listPizzaDb')],
+            [InlineKeyboardButton(text='PizzaOWL', callback_data='PizzaOWL')],
 
+        ])
+        )
+    update.message.reply_text(
+        "Procesamiento de PLN :\n"
+        "\n/PLN -> Procesamiento de lenguaje natural")
 
 def types_command_dbpedia(update, context):
     qres = dbpedia.get_response_dbpedia_pizzas()
     for i in range(len(qres['results']['bindings'])):
         result = qres['results']['bindings'][i]
         name, ing,  image_url = result['name']['value'], result['res']['value'], result['image']['value']
-        update.message.reply_text('Nombre de la pizza : ' + name + "\n Ingredientes: " + ing +
-                                 "\n" + image_url)
+        mensaje ='Nombre de la pizza : ' + name + "\n Ingredientes: " + ing +"\n" + image_url
+        update.callback_query.message.reply_text(mensaje)
 
 
 def types_command_owl(update, context):
@@ -76,32 +51,38 @@ def types_command_owl(update, context):
         result = qres['results']['bindings'][i]
         name = result['name']['value']
         qres2 = owl.get_response_ingredients(name)
-        update.message.reply_text('Nombre de la pizza : ' + name)
-        update.message.reply_text('ingredientes : ')
+        update.callback_query.message.reply_text('Nombre de la pizza : ' + name)
+        update.callback_query.message.reply_text('ingredientes : ')
         for j in range(len(qres2['results']['bindings'])):
             result2 = qres2['results']['bindings'][j]
             name2 = result2['name']['value']
-            update.message.reply_text(name2)
+            update.callback_query.message.reply_text(name2)
+
+
+def nlp_bot(update, context):
+    update.message.reply_text("Ingresa un texto")
+    mytxt = update.message.text  # obtener el texto que envio el usuario
+    print(mytxt)
+    doc = pln.spacy_info(mytxt)
+    for w in doc:
+        a = w.text, w.pos_
+        update.message.reply_text(a)
 
 
 if __name__ == '__main__':
+
     updater = Updater(token="", use_context=True)
 
     dp = updater.dispatcher
 
     # Commands
-    dp.add_handler(CommandHandler('start', start_command))
-    #dp.add_handler(CommandHandler('formenu', menu_command))
-    dp.add_handler(CommandHandler('help', help_command))
-    dp.add_handler(CommandHandler('listPizzaDb', types_command_dbpedia))
-    dp.add_handler(CommandHandler('PizzaOWL', types_command_owl))
-    #dp.add_handler(CommandHandler('pizza', pizza_command))
-    dp.add_handler(CommandHandler(
-        'ingredients', ingredients_command, pass_args=True))
 
+    dp.add_handler(CommandHandler('start', start_command))
+    dp.add_handler(CallbackQueryHandler(pattern='listPizzaDb', callback=types_command_dbpedia))
+    dp.add_handler(CallbackQueryHandler(pattern='PizzaOWL', callback=types_command_owl))
+    dp.add_handler(MessageHandler(Filters.text, nlp_bot))
 
     # Messages
-    #dp.add_handler(MessageHandler(Filters.text, handle_message))
 
     # Log all errors
     dp.add_error_handler(error)
@@ -109,3 +90,4 @@ if __name__ == '__main__':
     # Run the bot
     updater.start_polling(1.0)
     updater.idle()
+
